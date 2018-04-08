0a2e93f57aed0daa8e6768e5a248aca8dd1a4dbe
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-09 00:08:40 +0300

    'message'

---
+++ inventoryupdate.js
@@ -0,0 +1,25 @@
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+   
+    document.getElementById("array1").innerHTML=arr1;
+    document.getElementById("array2").innerHTML=arr2;
+
+    return arr1;
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+document.getElementById("result").innerHTML=updateInventory(curInv, newInv);
