a4cb1738db050b27753dbf286d04e1b927b8f98c
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-10 00:05:09 +0300

    first version

--- inventoryupdate.js
+++ inventoryupdate.js
@@ -1,14 +1,3 @@
-function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
-   
-    document.getElementById("array1").innerHTML=arr1;
-    document.getElementById("array2").innerHTML=arr2;
-
-    return arr1;
-}
-
-// Example inventory lists
-
 var curInv = [
     [21, "Bowling Ball"],
     [2, "Dirty Sock"],
@@ -23,4 +12,40 @@ var newInv = [
     [7, "Toothpaste"]
 ];
 
-document.getElementById("result").innerHTML=updateInventory(curInv, newInv);
+let finalArray=[];
+
+let showFinalArray = document.getElementById("result");
+
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+    let showArr1 = document.getElementById("array1");
+    let showArr2 = document.getElementById("array2");
+
+
+
+
+    arr1.forEach(function (item) {
+        showArr1.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
+    });
+    arr2.forEach(function (item) {
+        showArr2.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
+    });
+//not finnished - still has to add new items and update inventory
+    return finalArray = arr1.concat(arr2);
+
+};
+
+updateInventory(curInv, newInv);
+
+function getresult() {
+    finalArray.forEach(function (item) {
+        console.log(item);
+        showFinalArray.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
+    });
+};
+
+// Example inventory lists
+
+
+
+
