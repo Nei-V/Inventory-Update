49801772ee78478846964eed526b2b1450edae75
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-12 01:15:32 +0300

    still have to add new items

--- inventoryupdate.js
+++ inventoryupdate.js
@@ -21,7 +21,7 @@ function updateInventory(arr1, arr2) {
     let showArr1 = document.getElementById("array1");
     let showArr2 = document.getElementById("array2");
 
-    finalArray=[].concat(arr1);
+    finalArray = [].concat(arr1);
     console.log(finalArray);
     arr1.forEach(function (item) {
         showArr1.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
@@ -29,7 +29,7 @@ function updateInventory(arr1, arr2) {
     arr2.forEach(function (item) {
         showArr2.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
     });
-    //not finnished - still has to add new items and update inventory
+    //  update inventory if item found - done. Still to do add new items
 
     arr1.forEach(function (item) {
         function checkandUpdateArr1() {
@@ -39,6 +39,13 @@ function updateInventory(arr1, arr2) {
             //console.log("arr2[1]",arr2[1]);
             if (arr2.find(k => k[1] == item[1]) !== undefined) {
                 console.log(arr2.find(k => k[1] == item[1]));
+                let foundItem = arr2.find(k => k[1] == item[1]);
+                console.log("position in arr1", finalArray[arr1.indexOf(item)]);
+                finalArray[arr1.indexOf(item)][0] = item[0] + foundItem[0];
+            }
+            else {//not wroking yet)
+                arr2.find(k => k[1] == item[1])
+                console.log("value of not found",valueNotFound);
             };
         };
         checkandUpdateArr1();
