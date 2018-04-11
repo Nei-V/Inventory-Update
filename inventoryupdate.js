11534be7ae507fbb40bd21c14d36fbbea674ce90
Ion Varsescu <19718355+Nei-V@users.noreply.github.com>
2018-04-12 01:27:50 +0300

    still just to sort final array

--- inventoryupdate.js
+++ inventoryupdate.js
@@ -21,7 +21,7 @@ function updateInventory(arr1, arr2) {
     let showArr1 = document.getElementById("array1");
     let showArr2 = document.getElementById("array2");
 
-    finalArray = [].concat(arr1);
+    finalArray = [].concat(arr2);
     console.log(finalArray);
     arr1.forEach(function (item) {
         showArr1.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
@@ -29,7 +29,7 @@ function updateInventory(arr1, arr2) {
     arr2.forEach(function (item) {
         showArr2.innerHTML += "<tr>" + "<td>" + item[0] + "</td>" + "<td>" + item[1] + "</td>" + "</tr>";
     });
-    //  update inventory if item found - done. Still to do add new items
+    //  has to sort finalArray
 
     arr1.forEach(function (item) {
         function checkandUpdateArr1() {
@@ -43,8 +43,8 @@ function updateInventory(arr1, arr2) {
                 console.log("position in arr1", finalArray[arr1.indexOf(item)]);
                 finalArray[arr1.indexOf(item)][0] = item[0] + foundItem[0];
             }
-            else {//not wroking yet)
-               
+            else {
+               finalArray.push(item);
             };
         };
         checkandUpdateArr1();
